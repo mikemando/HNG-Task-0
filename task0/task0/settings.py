c913d3126e3035c107535519ec036ecc030f4ebb
mikemando <ikwuegbu300@gmail.com>
2025-01-29 15:22:21 +0100

    enable vercel deployment

--- task0/task0/settings.py
+++ task0/task0/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-a=k4*0edu((zv1u&cwmqrg%r$f-b!!d5nx=thrfc5e=t1z#b))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
