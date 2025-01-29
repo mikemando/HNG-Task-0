168acb8f1045b4c31c661a40b29d5290e257ed68
mikemando <ikwuegbu300@gmail.com>
2025-01-29 17:23:37 +0100

    format dateTime output

--- solution/views.py
+++ solution/views.py
@@ -1,9 +1,13 @@
 from django.http import JsonResponse
 from datetime import datetime, timezone
 
+
 def home(request):
+    currDateTime = datetime.now(timezone.utc).replace(microsecond=0).isoformat()
+    timeStamp = currDateTime.split('+')[0] + 'Z'
+
     return JsonResponse({
         "email": "ikwuegbu300@gmail.com",
-        "current_datetime": datetime.now(timezone.utc).isoformat(),
+        "current_datetime": timeStamp,
         "github_url": "https://github.com/mikemando/HNG-Task-0"
     })
